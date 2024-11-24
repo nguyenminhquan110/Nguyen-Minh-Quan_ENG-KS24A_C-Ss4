8ec4c0d21d182b18cb88128418782576260c2366
nguyenminhquan110 <nmquan11026@gmail.com>
2024-11-24 22:31:23 +0700

    Add files via upload

---
+++ Ss4_7.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int year;
+    printf("Nhap vao mot nam: ");
+    scanf("%d", &year);
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("Nam %d la nam nhuan.\n", year);
+    } else {
+        printf("Nam %d khong phai là nam nhuan.\n", year);
+    }
+    return 0;
+}
+
