0814f89dc6f6ad868f8629dc58c18bb181a042b5
nguyenminhquan110 <nmquan11026@gmail.com>
2024-11-24 19:30:14 +0700

    Add files via upload

---
+++ Ss4_2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int number;
+	printf("Moi ban nhap so nguyen: ");
+	scanf("%d", &number);
+	if(number %2== 0){
+		printf("%d la so chan.\n",number); 
+	}else {
+	printf("%d la so le.\n",number); 
+	}
+	return 0; 
+} 
